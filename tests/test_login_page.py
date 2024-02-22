5f763c5ca0f4e2aa797f5c6e0a3e4bc2d03369b5
Roberto Zumba Naranjo <49047445+na001988@users.noreply.github.com>
2024-02-22 13:04:11 +1100

    Update test_login_page.py

--- tests/test_login_page.py
+++ tests/test_login_page.py
@@ -22,12 +22,11 @@ class TestPositiveScenarios:
         wait.until(EC.url_to_be(base_url))
 
         # Type username student into Username field
-        # username_locator = driver.find_element(By.ID, "username")
         wait.until(EC.presence_of_element_located(
-            (By.ID, "username")
+            (By.ID, "username").send_keys("student")
         ))
         
-        driver.find_element(By.ID, "username").send_keys("student")
+        #driver.find_element(By.ID, "username").send_keys("student")
 
         # Type password Password123 into Password field
         password_locator = driver.find_element(By.NAME, "password")
